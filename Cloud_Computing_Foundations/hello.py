3cc0f95f975fe16f19f4f267d6d0b174ad0a1a46
KhaiKit <wo0001it@e.ntu.edu.sg.com>
2023-03-09 17:45:01 +0000

    updated all code

--- Cloud_Computing_Foundations/hello.py
+++ Cloud_Computing_Foundations/hello.py
@@ -1,5 +1,8 @@
-def add(x, y):
-    return x + y
-    
-result = add(1,2)
-print(f"This is the sum: {result}")
+def toyou(x):
+    return f"hi {x}"
+
+def add(x):
+    return x + 1
+
+def subtract(x):
+    return x - 1
